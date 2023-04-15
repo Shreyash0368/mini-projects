528ed4e2e66a9398595b3f9e240baa8e4a911982
Shreyash0368 <shreyashgpt0503@gmail.com>
2023-04-15 09:32:32 +0530

    ticket removal,sorting and local storage and improve in readability

--- JIRATicketManagement/scripts/script.js
+++ JIRATicketManagement/scripts/script.js
@@ -1,16 +1,38 @@
-// acquiring global objects
+//* acquiring global objects
+
+// Aqcuiring add and remove button
 let addButton = document.querySelector('.add');
 let removeButton = document.querySelector('.remove');
-let addFlag = false;
-let removeFlag = false;
+
 let noteCreatorText = document.querySelector('.textarea-cont');
 let noteCreator = document.querySelector('.note-creator');
+
+// remove button flags
+let addFlag = false;
+let removeFlag = false;
+
+// values for setting color properties
 let colorArray = ['lightpink', 'lightblue', 'lightgreen', 'black'];
 let colorOptions = document.querySelectorAll('.color-pick');
 
+// array of notes 
+let noteArray = [];
+
+// //? loading old notes from local storage
+if (localStorage.getItem('jiraTicket')) {
+    noteArray = JSON.parse(localStorage.getItem('jiraTicket'));   
+    noteArray.forEach((note,idx) => {
+        ticketCreator(note.color, note.text, note.ID.substring(1,7), false);
+    })    
+}
+
+console.log(noteArray);
 
 
-// event listner for displaying the note creator
+
+// ! adding and removing notes
+
+//? event listner for displaying the note creator
 addButton.addEventListener('click', (e) => {
     addFlag = !addFlag;
     removeFlag = false;
@@ -23,29 +45,28 @@ addButton.addEventListener('click', (e) => {
     }
 })
 
+//?  event listner for node removal
 removeButton.addEventListener('click', (e) => {
     removeFlag = !removeFlag;
     if (removeFlag) {
-        removeButton.style.backgroundColor= '#6a89cc';
+        removeButton.style.backgroundColor = '#6a89cc';
     }
     else {
-        removeButton.style.backgroundColor= '#2f3542';       
+        removeButton.style.backgroundColor = '#2f3542';
     }
-    console.log(removeFlag);
+
     let allNotes = document.querySelectorAll('.note')
     allNotes.forEach((note) => {
         note.addEventListener('click', (e) => {
             if (removeFlag) {
-                note.remove()
+                ticketRemover(note)
             }
         })
     })
-    
+
 })
 
-
-
-// event listner for adding new note 
+//? event listner for adding new note 
 noteCreatorText.addEventListener('keydown', (e) => {
     let shiftPress = e.shiftKey;
     if (e.key === 'Enter' && shiftPress) {
@@ -56,44 +77,79 @@ noteCreatorText.addEventListener('keydown', (e) => {
         let ticketID = crypto.randomUUID().substring(1, 6);
 
         // generating ticket
-        ticketCreator(ticketColor, ticketCont, ticketID);
+        ticketCreator(ticketColor, ticketCont, ticketID, true);
 
         // removing display of note creator and reseting all values
         addFlag = false;
         noteCreator.style.display = 'none';
+
+        // setting all values to default
         noteCreatorText.value = null;
         colorOptions.forEach(element => {
             element.classList.remove('border');
         });
         colorOptions[3].classList.add('border');
 
-        
+
     }
 })
 
+// ! sorting based on display
+let allColorChoices = document.querySelectorAll('.colorChoice');
+allColorChoices.forEach((choice) => {
+    choice.addEventListener('click', (e) => {
+        // getting color of the option clicked on
+        let colorChoosed = e.target.classList.value.split(' ')[0]
+
+        let allNoteColors = document.querySelectorAll('.note-color')
+        allNoteColors.forEach(note => {
+            // getting all classes in the color div and splitting the result string based on ' ' (space) which gives an array as result
+            let colorOfNote = note.classList.value.split(' ')[1];
+            let parentElmnt = note.parentElement;
+            if (colorChoosed === colorOfNote) {
+                parentElmnt.style.display = 'block';
+            }
+            else {
+                parentElmnt.style.display = 'none';
+            }
+
+        })
+    })
 
 
-// event listner for node removal
+    choice.addEventListener("dblclick", (e) => {
+        let noteArray = document.querySelectorAll('.note');
+        noteArray.forEach(note => {
+            note.style.display = 'block';
+        })
+    })
+})
 
 
+// ! functions
 
+//* ticket creation function
+function ticketCreator(ticketColor, ticketText, ticketID, newTicket) {
+    
 
-// ticket creation function
-function ticketCreator(ticketColor, ticketText, ticketID) {
     let noteConatiner = document.querySelector('.note-container');
     let note = document.createElement('div');
     note.classList.add('note');
     note.innerHTML = `
             <div class="note-color ${ticketColor} round-border"></div>
             <div class="note-ID">#${ticketID}</div>
-            <div class="note-text round-border">${ticketText}</div> 
-        `
+            <div class="note-text round-border">${ticketText}</div>         `
     noteConatiner.appendChild(note);
+
+    if (newTicket) {
+        noteArray.push({ color: ticketColor, ID: `#${ticketID}`, text: ticketText });
+        console.log(noteArray);    
+        localStorage.setItem('jiraTicket', JSON.stringify(noteArray));
+    }
+    
 }
 
-// ticket removal 
-
-// getting the color value choosed by user
+//* getting the color value choosed by user
 function getColor(colorChoice) {
     for (let index = 0; index < colorChoice.length; index++) {
         if (colorChoice[index].classList.contains('border')) {
@@ -102,7 +158,7 @@ function getColor(colorChoice) {
     }
 }
 
-// setting border for choosen color
+//* setting border for choosen color
 colorOptions.forEach((color) => {
     color.addEventListener('click', (e) => {
         colorOptions.forEach(element => {
@@ -112,3 +168,14 @@ colorOptions.forEach((color) => {
     })
 })
 
+function ticketRemover(note) {
+    let noteId = note.querySelector('.note-ID').innerText;
+    for (let index = 0; index < noteArray.length; index++) {
+        if (noteArray[index].ID === (noteId) ) {
+            noteArray.splice(index, 1);
+            console.log(noteArray);
+        }
+    }
+    localStorage.setItem('jiraTicket', JSON.stringify(noteArray));    
+    note.remove()
+}
